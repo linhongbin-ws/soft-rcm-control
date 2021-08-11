09327f68d5f49bd8e9a5a7dec25186489d86500f
ben <522181941@qq.com>
2021-08-11 15:48:46 +0800

    [tmp]

--- teleop/teleopRCM.m
+++ teleop/teleopRCM.m
@@ -1,12 +1,6 @@
 classdef teleopRCM < handle
   properties(Access = public)
 
-    isStarted = false;
-    psm_js_publisher;
-    jointStateMsg;
-    dt = 0.01;
-    tStart = tic;
-
     %%% general
     time_delta = 0.01; % 100hz
     duration = 5;
@@ -19,7 +13,6 @@ classdef teleopRCM < handle
     loops_per_plots = 10; % render every n control loop
 
     %%% control
-    map_R = []; % empty if track current R
     transl_scale = 0.5; %mapping scale for translation
     lamda_rcm0 = 0.5; %set inital rcm point with lamda
     tracking_gain_transl = 200; %control gain for tracking PD control
@@ -34,9 +27,14 @@ classdef teleopRCM < handle
     q0_slave = deg2rad([30;30;30;30;30;30;30]); % slave intial q
     q0_slave_wrist =  deg2rad([0,0,0].'); 
     
+    %%% dvrk version
+    dvrk_version = 2;
+    
     % variables
     qs_slave
     qs_slave_wrist
+    qt_slave;
+    qt_slave_wrist
     T0_master 
     lamda_rcm
     qdott_slave
@@ -44,138 +42,178 @@ classdef teleopRCM < handle
     ds = [];
     error_transl_norms = [];
     error_rot_norms = [];
-    map_R 
     is_init = false;
     Tt_master
     Tt_mns_1_master
     rcm_p0
     rcm_p_drift
-
+    pub_slave
+    pub_slave_wrist
+    sub_master
+    sub_slave
+    sub_slave_wrist
+    is_initial_loop_teleop
+    T0_slave
+    T0_slave_wrist
+    Tt_slave_jnts
+    Tt_slave_wrist_jnts
+    Jt_slave_s
+    Jt_slave_wrist
+    Tt_slave
+    Tt_slave_jnts_all
+    qt_slave_dsr
+    qt_slave_wrist_dsr
+    qdott_slave_wrist
+    error_transl_norm
+    error_rot_norm
+    rcm_p
+    map_R 
+    T0_slave_tip
   end
 
     methods(Access = public)
 
         function obj = teleopRCM()  % Constructor
-            [T0_slave,~] = fk_geom(q0_slave,model_slave.table, model_slave.tip, model_slave.method,false,[]); % get initial slave T
-            [T0_slave_wrist,~] = fk_geom(q0_slave_wrist,model_slave_wrist.table, model_slave_wrist.tip, model_slave_wrist.method,false,[]); % get initial slave wrist T
 
-
-            Ts_master = mtm_x;
-            % %%% additional pseudo tracking for master
-            % Ts_master = [];
-            % for i = 1:size(mtm_x,3)
-            %     Ts_master = cat(3, Ts_master, mtm_x(:,:,i) * rot_T(deg2rad(i/5), 'z'));
-            % end
-
-
-            % variables
-            % Ts_master = mtm_x; 
-            qs_slave = [q0_slave];
-            qs_slave_wrist = [q0_slave_wrist];
-            T0_master = Ts_master(:,:,1);
-            lamda_rcm = lamda_rcm0;
-            qdott_slave = zeros(7,1);
-            rcm_ps = [];
-            ds = [];
-            error_transl_norms = [];
-            error_rot_norms = [];
-
-            %%% use inital R as mapping R (one way to reduce rotational tracking error)
-            if isempty(map_R)
-                T0_slave_tip = T0_slave*T0_slave_wrist;
-                map_R =  inv(T0_master(1:3,1:3)) * T0_slave_tip(1:3,1:3); %
+            obj.idle_topics()
+        end
+        
+        function master_cb(obj, transform)
+            rotm = quat2rotm(transform.rotation);
+            posv = transform.position;
+            if ~isempty(obj.Tt_master)
+                obj.Tt_mns_1_master = obj.Tt_master;
             end
+            obj.Tt_master = [rotm, posv; zeros(1,3), 1];
 
         end
-
-        %mtm_q is MTM joint position, a 7x1 vector
-        function  [psm_q,tracking_err] = run(obj, Tt_master)
-            if obj.is_init
-                % do stuff
-                obj.Tt_mns_1_master = Tt_master;
-                obj.rcm_p = [];
-                obj.rcm_p0 = [];
-                obj.T0_master = Tt_master;
-            end
-            
-            obj.Tt_master = Tt_master;
-            
-             %%% update current state
-%             obj.qt_slave = qs_slave(:,end);
-%             obj.qt_slave_wrist = qs_slave_wrist(:,end);
-%            Ts_master = cat(3, Ts_master, Tt_master);
-            
-
-            [Tt_slave_jnts, Jt_slave_s] =fk_geom(qt_slave, obj.model_slave.table, obj.model_slave.tip, obj.model_slave.method, true, [obj.model_slave.rcm_top_jnt_idx, obj.model_slave.rcm_tip_jnt_idx]);
-            [Tt_slave_wrist_jnts, Jt_slave_wrist] =fk_geom(qt_slave_wrist, obj.model_slave_wrist.table, obj.model_slave_wrist.tip, obj.model_slave_wrist.method, true, []);
-            Tt_slave = Tt_slave_jnts(:,:,end);
-            Tt_slave_jnts_all = Tt_slave_jnts;
-            for k = 1:size(Tt_slave_wrist_jnts,3)
-                Tt_slave_jnts_all = cat(3, Tt_slave_jnts_all, Tt_slave*Tt_slave_wrist_jnts(:,:,k));
-            end
-
-            %%% update lamda_rcm
-            rcm_top_T = Tt_slave_jnts(:,:,obj.model_slave.rcm_top_jnt_idx+1);
-            rcm_tip_T = Tt_slave_jnts(:,:,obj.model_slave.rcm_tip_jnt_idx+1);
-            stick_length = norm(rcm_tip_T(1:3,4) - rcm_top_T(1:3,4));
-            rcm_top_jacob = Jt_slave_s(:,:,2) ;
-            rcm_tip_jacob = Jt_slave_s(:,:,3) ;
-            rcm_jacob = rcm_top_jacob * (1-obj.lamda_rcm) + rcm_tip_jacob*obj.lamda_rcm;
-            zv = dot(rcm_jacob(1:3,:)*obj.qdott_slave,  rcm_top_T(1:3,3));
-            obj.lamda_rcm  =obj.lamda_rcm - (zv * obj.time_delta)/stick_length;
-            obj.rcm_p = rcm_top_T(1:3,4)*(1-obj.lamda_rcm) + rcm_tip_T(1:3,4)*obj.lamda_rcm;
-            if obj.is_init
-                obj.rcm_p_drift = norm(obj.rcm_p-obj.rcm_p0);
-            end
-
-%             %%% plot render
-%             if(mod(i,loops_per_plots) == 1)
-%                  rcm_Tip_T = Tt_slave_jnts(:,:,model_slave.rcm_tip_jnt_idx+1);
-%                  rcm_p = rcm_top_T(1:3,4)*(1-lamda_rcm) + rcm_Tip_T(1:3,4)*lamda_rcm;
-%                  joints_render_master_slave(Tt_master, Tt_slave_jnts_all, xlims, ylims, zlims, arms_offsets, rcm_p, true); % visualize
-%             end
-
-            %%% calculate mapping
-            Tt_slave_dsr = map_MTM2Flexiv(Tt_master, obj.map_R, obj.T0_master, obj.T0_slave, obj.transl_scale);
-            [Tt_err_slave, ~] = error_T(obj.Tt_slave, obj.Tt_slave_dsr); % get positional error
-            [vel_master, ~] = error_T(obj.Tt_master,obj.obj.Tt_mns_1_master);
-            vt_dsr_slave = [obj.map_R*vel_master(1:3);obj.map_R*vel_master(4:6)]; % get desired velocity
-
-        %     iTt_slave = inv(Tt_slave);
-            tmp = map_MTM2Flexiv(Tt_master, map_R, T0_master, T0_slave_tip, transl_scale);
-            [tmp, ~] = error_T(Tt_slave_jnts_all(:,:,end),tmp); % get positional error
-            Tt_err_slave_wrist = [obj.Tt_slave(1:3,1:3).'*tmp(1:3,:);Tt_slave(1:3,1:3).'*tmp(4:6,:)];
-            vt_dsr_slave_wrist = [obj.Tt_slave(1:3,1:3).'*vt_dsr_slave(1:3,:);Tt_slave(1:3,1:3).'*vt_dsr_slave(4:6,:)];
-
-
-            % calculate contrain jacobian
-            H = contrained_Jacob(Tt_slave_jnts(:,:,model_slave.rcm_top_jnt_idx+1), Jt_slave_s(:,:,2), Jt_slave_s(:,:,3), lamda_rcm);
-
-
-        %     vt_slave = control_inv_jacob_redundant(Tt_err_slave, vt_dsr_slave, Jt_slave, lambda, zeros(7,1)); % inverse jacobian control for redundant robot
-        %      vt_slave = control_inv_jacob_rdd_pos(Tt_err_slave, vt_dsr_slave, Jt_slave, lambda, zeros(7,1)); % inverse jacobian control for redundant robot
-            [qdott_slave, error_norm] = control_inv_jacob_rdd_pos_rcm(Tt_err_slave, vt_dsr_slave, Jt_slave_s(:,:,1), H, lambda_transl, zeros(7,1), tracking_gain_transl); % inverse jacobian control for redundant robot
-            error_transl_norms = [error_transl_norms, error_norm];
-            [qdott_slave_wrist, error_norm] = control_inv_jacob_rot(Tt_err_slave_wrist,vt_dsr_slave_wrist,Jt_slave_wrist,lambda_rot, tracking_gain_rot);
-            error_rot_norms = [error_rot_norms, error_norm];
-            qs_slave= [qs_slave, qt_slave+qdott_slave*time_delta]; % step velocity for flexiv
-            qs_slave_wrist = [qs_slave_wrist, qt_slave_wrist+qdott_slave_wrist*time_delta]; % step velocity for wrist
-            
-            if obj.is_init
-                obj.is_init = false;
-            end
- 
+        
+        function slave_idle_cb(obj, q)
+            obj.qt_slave = q;
+        end
+        
+        function slave_teleop_cb(obj,q)
+            obj.qt_slave = q;
+            obj.run(q);
+            obj.qt_slave_dsr
+            obj.psm_js_publisher.send(obj.jointStateMsg);
         end
 
-        function  callback_update_mtm_q(obj,q)
-            obj.jointStateMsg.Position = obj.run(q);
-            %obj.jointStateMsg.Position
-            tElapsed = toc(obj.tStart);
-            if (tElapsed > 0.033)
-                obj.tStart = tic;
-                obj.psm_js_publisher.send(obj.jointStateMsg);
+        
+        function idle_topics(obj)
+            if obj.dvrk_version == 2
+
+                % master subscribe
+                sub_master_cb = @(src,msg)(obj.master_cb(msg.transform));
+                obj.sub_master = rossubscriber('/MTML/measured_cp',sub_master_cb,'BufferSize',2);
+        
+                % slave subscribe
+                sub_slave_idle_cb = @(src,msg)(obj.slave_idle_cb(msg.position));
+                obj.sub_slave = rossubscriber('/dvrk/MTML/state_joint_current',sub_slave_idle_cb,'BufferSize',2);
+                
+                % slave publish
+                obj.pub_slave = rospublisher('xxx','std_msgs/JointState');
+                obj.pub_slave_wrist = rospublisher('xxxa','std_msgs/JointState');
+                
+            else
+                error('not support')
             end
         end
+        
+        function teleop_topics(obj)
+            if obj.dvrk_version == 2
+
+                % master subscribe
+                sub_master_cb = @(src,msg)(obj.master_cb(msg.transform));
+                obj.sub_master = rossubscriber('/MTML/measured_cp',sub_master_cb,'BufferSize',2);
+        
+                % slave subscribe
+                sub_slave_teleop_cb = @(src,msg)(obj.master_cb(msg.position));
+                obj.sub_slave = rossubscriber('/dvrk/MTML/state_joint_current',sub_slave_teleop_cb,'BufferSize',2);
+                
+                % slave publish
+                obj.pub_slave = rospublisher('xxx','std_msgs/JointState');
+                obj.pub_slave_wrist = rospublisher('xxxa','std_msgs/JointState');
+            else
+                error('not support')
+            end
+        end
+        
+        function reset(obj)
+            obj.rcm_p = [];
+            obj.rcm_p0 = [];
+            obj.Tt_mns_1_master = [];
+            obj.Tt_master = [];
+            obj.T0_master = [];
+            obj.T0_master = [];
+            obj.lamda_rcm = obj.lamda_rcm0;
+            obj.qdott_slave = [];
+            obj.map_R = [];
+            
+
+            
+        end
+        
+        function start_teleop(obj)
+            obj.is_initial_loop_teleop = true;
+            obj.teleop_topics();
+        end
+        
+        function stop_teleop(obj)
+            obj.idle_topics();
+        end
+
+
+        function  run(obj)
+            
+            if obj.is_initial_loop_teleop
+                obj.q0_slave = obj.qt_slave;
+                obj.q0_slave_wrist = obj.qt_slave_wrist; 
+                [obj.T0_slave,~] = fk_geom(obj.q0_slave,obj.model_slave.table, obj.model_slave.tip, obj.model_slave.method,false,[]); % get initial slave T
+                [obj.T0_slave_wrist,~] = fk_geom(obj.q0_slave_wrist,obj.model_slave_wrist.table, obj.model_slave_wrist.tip, obj.model_slave_wrist.method,false,[]); % get initial slave wrist T
+                obj.T0_master = obj.Tt_master; 
+                
+                %%% use inital R as mapping R (one way to reduce rotational tracking error)
+                obj.T0_slave_tip = obj.T0_slave*obj.T0_slave_wrist;
+                obj.map_R =  obj.T0_master(1:3,1:3).' * obj.T0_slave_tip(1:3,1:3); 
+         
+                return
+            end
+
+            
+            %%% slave kinematics
+            [obj.Tt_slave_jnts, obj.Jt_slave_s] =fk_geom(obj.qt_slave, obj.model_slave.table, obj.model_slave.tip, obj.model_slave.method, true, [obj.model_slave.rcm_top_jnt_idx, obj.model_slave.rcm_tip_jnt_idx]);
+            [obj.Tt_slave_wrist_jnts, obj.Jt_slave_wrist] =fk_geom(obj.qt_slave_wrist, obj.model_slave_wrist.table, obj.model_slave_wrist.tip, obj.model_slave_wrist.method, true, []);
+            obj.Tt_slave = obj.Tt_slave_jnts(:,:,end);
+            tmp = obj.Tt_slave_jnts;
+            for k = 1:size(obj.Tt_slave_wrist_jnts,3)
+                tmp = cat(3, tmp, obj.Tt_slave*obj.Tt_slave_wrist_jnts(:,:,k));
+            end
+            obj.Tt_slave_jnts_all = tmp;
+
+
+            %%% mapping
+            Tt_slave_mapped = map_MTM2Flexiv(obj.Tt_master, obj.map_R, obj.T0_master, obj.T0_slave, obj.transl_scale);
+            [Tt_err_slave, ~] = error_T(obj.Tt_slave, Tt_slave_mapped); % get positional error
+            [vel_master, ~] = error_T(obj.Tt_master,obj.Tt_mns_1_master);
+            vt_slave_mapped = [obj.map_R*vel_master(1:3);obj.map_R*vel_master(4:6)]; % get desired velocity
+            tmp = map_MTM2Flexiv(obj.Tt_master, obj.map_R, obj.T0_master, obj.T0_slave_tip, obj.transl_scale);
+            [tmp, ~] = error_T(obj.Tt_slave_jnts_all(:,:,end),tmp); % get positional error
+            Tt_err_slave_wrist = [obj.Tt_slave(1:3,1:3).'*tmp(1:3,:);obj.Tt_slave(1:3,1:3).'*tmp(4:6,:)];
+            vt_slave_wrist_mapped = [obj.Tt_slave(1:3,1:3).'*vt_slave_mapped(1:3,:);obj.Tt_slave(1:3,1:3).'*vt_slave_mapped(4:6,:)];
+
+
+            %%% control
+            H = contrained_Jacob(obj.Tt_slave_jnts(:,:,obj.model_slave.rcm_top_jnt_idx+1), obj.Jt_slave_s(:,:,2), obj.Jt_slave_s(:,:,3), obj.lamda_rcm); % contrain jacobian
+            [obj.qdott_slave, obj.error_transl_norm] = control_inv_jacob_rdd_pos_rcm(Tt_err_slave, vt_slave_wrist_mapped, obj.Jt_slave_s(:,:,1), H, obj.lambda_transl, zeros(7,1), obj.tracking_gain_transl); % inverse jacobian control for redundant robot
+            [obj.qdott_slave_wrist, obj.error_rot_norm] = control_inv_jacob_rot(Tt_err_slave_wrist,vt_slave_wrist_mapped,obj.Jt_slave_wrist,obj.lambda_rot, obj.tracking_gain_rot);
+
+            
+            %%% slave desired q
+            obj.qt_slave_dsr = obj.qt_slave+obj.qdott_slave*obj.time_delta;
+            obj.qt_slave_wrist_dsr = obj.qt_slave+obj.qdott_slave*obj.time_delta;
+        end
+
 
     end
 end
